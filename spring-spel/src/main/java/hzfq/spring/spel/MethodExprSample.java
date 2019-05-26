924ab115f22cc50c9d116c1e20e3b90c246ae33c
huzhifengqing <1443754157@qq.com>
2019-05-26 23:05:49 +0800

    方法解析

--- spring-spel/src/main/java/hzfq/spring/spel/MethodExprSample.java
+++ spring-spel/src/main/java/hzfq/spring/spel/MethodExprSample.java
@@ -1,11 +1,23 @@
 package hzfq.spring.spel;
 
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
 /**
- *
+ * 方法解析：支持Java可访问的方法，包括对象方法、静态方法、可变方法参数，String类型的所有可访问方法
+ * 
  * @author huzhifengqing
  * @email huzhifengqing@qq.com
  * @data 2019年5月26日
  */
 public class MethodExprSample {
 
+    public static void main(String[] args) {
+        ExpressionParser parser = new SpelExpressionParser();
+
+        String substring = parser.parseExpression("'Spring SpEL'.substring(7)").getValue(String.class);
+        Integer index = parser.parseExpression("'Spring SpEL'.indexOf('SpEL')").getValue(Integer.class);
+        System.out.println(substring);
+        System.out.println(index);
+    }
 }
